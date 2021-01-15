66d1b5340e979f4336a01a89d48463d49b01ece3
Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
2021-01-15 09:27:19 +0100

    Fix copyright header

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
+ * Copyright (C) 2020–21 Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
